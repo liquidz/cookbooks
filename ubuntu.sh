5555c13f4b72577c02ba9c85e01ec3e0bc20b64c
liquidz <liquidz.uo@gmail.com>
2016-10-23 23:06:58 +0900

    update

---
+++ ubuntu.sh
@@ -0,0 +1,25 @@
+#1/bin/sh
+
+sudo apt-get update && \
+    sudo apt-get upgrade
+
+sudo apt-get install -y \
+    zsh tmux docker.io \
+    openjdk-8-jdk \
+    software-properties-common
+
+# zsh
+chsh -s /bin/zsh
+
+# leiningen
+curl -sfLo ~/bin/lein --create-dirs \
+ https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod +x ~/bin/lein
+
+# neovim
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+# dotfiles
+curl -L http://bit.ly/uochan_dotfile | bash
