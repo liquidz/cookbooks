3c6dca52b29d75f1574dfd16a21444a2a134d3b9
liquidz <liquidz.uo@gmail.com>
2016-11-14 14:15:04 +0000

    :cat:

--- cookbooks/rust/develop.rb
+++ cookbooks/rust/develop.rb
@@ -1,8 +1,21 @@
 include_recipe 'default'
 
+rust_version = '1.13.0'
+
 %w( rustfmt racer ).each do |pkg_name|
   execute "cargo install #{pkg_name}" do
     command "cargo install #{pkg_name}"
     not_if "test -e ~/.cargo/bin/#{pkg_name}"
   end
+end
+
+name = "rustc-#{rust_version}"
+archive = "#{name}-src.tar.gz"
+src_url = "https://static.rust-lang.org/dist/#{archive}"
+execute 'download rust src' do
+  cwd '~/src'
+  command <<-EOC
+    wget #{src_url} && tar xvf #{archive}
+  EOC
+  not_if "~/src/#{name}"
 end
--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -1,40 +1,40 @@
 defaults = {
-    packages: %W( make gcc git ncurses-dev python-dev python3-dev ),
-    src_dir: '/usr/local/src/vim',
-    configure: %W( --prefix=/usr/local
-                   --with-features=huge
-                   --enable-multibyte
-                   --enable-pythoninterp
-                   --enable-fail-if-missing).join(' '),
+  packages: %W( build-essential git ncurses-dev python-dev python3-dev ),
+  src_dir: '/usr/local/src/vim',
+  configure: %W( --prefix=/usr/local
+                 --with-features=huge
+                 --enable-multibyte
+                 --enable-pythoninterp
+                 --enable-fail-if-missing).join(' '),
 }
 
 define :vim_from_source, defaults do
-    package 'vim' do
-        action :remove
-    end
-    params[:packages].each do |pkg_name|
-        package pkg_name
-    end
+  package 'vim' do
+    action :remove
+  end
+  params[:packages].each do |pkg_name|
+    package pkg_name
+  end
 
-    git params[:src_dir] do
-      repository 'https://github.com/vim/vim'
-    end
+  git params[:src_dir] do
+    repository 'https://github.com/vim/vim'
+  end
 
-    execute 'configure' do
-      cwd params[:src_dir]
-      command "./configure #{params[:configure]}"
-      not_if 'test -e src/auto/config.log'
-    end
+  execute 'configure' do
+    cwd params[:src_dir]
+    command "./configure #{params[:configure]}"
+    not_if 'test -e src/auto/config.log'
+  end
 
-    execute 'make and make install' do
-      cwd params[:src_dir]
-      command 'make && make install'
-      not_if 'test -e /usr/local/bin/vim'
-    end
+  execute 'make and make install' do
+    cwd params[:src_dir]
+    command 'make && make install'
+    not_if 'test -e /usr/local/bin/vim'
+  end
 
-    template '/usr/local/src/vim/rebuild.sh' do
-      source 'rebuild.sh.erb'
-      mode '0755'
-      variables(defaults)
-    end
+  template '/usr/local/src/vim/rebuild.sh' do
+    source 'rebuild.sh.erb'
+    mode '0755'
+    variables(defaults)
+  end
 end
