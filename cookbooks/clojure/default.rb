b8e5ad6f6a87baae30587bf4d2d5b0bea9b836d7
liquidz <liquidz.uo@gmail.com>
2017-08-16 01:57:11 +0000

    clojure関連のレシピをrootユーザから実行できるよう修正

--- cookbooks/clojure/default.rb
+++ cookbooks/clojure/default.rb
@@ -1,14 +1,21 @@
 include_recipe '../def'
 
-target = "#{ENV['HOME']}/bin/lein"
+bin_dir = "/home/#{node[:user]}/bin"
+target = "#{bin_dir}/lein"
 
-directory "#{ENV['HOME']}/bin"
-curl 'download leiningen' do
+directory bin_dir do
+  owner node[:user]
+  group node[:group]
+end
+
+curl 'leiningen' do
   path target
   url  'https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein'
 end
 
 file target do
   mode '0755'
+  owner node[:user]
+  group node[:group]
 end
 
---
+++ cookbooks/clojure/joker.rb
@@ -0,0 +1,28 @@
+include_recipe '../def'
+
+version  = '0.8.2'
+bin_dir  = "/home/#{node[:user]}/bin"
+zip_file = "/tmp/joker.zip"
+target   = "#{bin_dir}/joker"
+
+directory bin_dir do
+  owner node[:user]
+  group node[:group]
+end
+
+curl 'joker.zip' do
+  path zip_file
+  url  "https://github.com/candid82/joker/releases/download/v#{version}/joker-#{version}-linux-amd64.zip"
+end
+
+execute 'unzip joker' do
+  cwd bin_dir
+  command "unzip #{zip_file}"
+  not_if "test -e #{target}"
+end
+
+file "#{bin_dir}/joker" do
+  mode '0755'
+  owner node[:user]
+  group node[:group]
+end
