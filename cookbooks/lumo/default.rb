b7a6a7c5ab19c9f87fadd2218d5ec64c5b9e1461
liquidz <liquidz.uo@gmail.com>
2017-08-29 08:59:44 +0000

    update lumo's recipe

--- cookbooks/lumo/default.rb
+++ cookbooks/lumo/default.rb
@@ -1,25 +1,6 @@
-include_recipe '../def'
+include_recipe '../yarn/default'
 
-version = '1.6.0'
-bin_dir = "/home/#{node[:user]}/bin"
-
-curl 'lumo' do
-  path '/tmp/lumo.zip'
-  url  "https://github.com/anmonteiro/lumo/releases/download/#{version}/lumo_linux64.zip"
-end
-
-directory bin_dir do
-  owner node[:user]
-  group node[:group]
-end
-
-execute 'unzip' do
-  cwd bin_dir
-  command 'unzip /tmp/lumo.zip'
-  not_if "test -e #{bin_dir}/lumo"
-end
-
-file "#{bin_dir}/lumo" do
-  owner node[:user]
-  group node[:group]
+execute 'install lumo-cljs' do
+  command 'yarn global add lumo-cljs'
+  not_if 'which lumo'
 end
