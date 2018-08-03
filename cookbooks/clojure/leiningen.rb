40fa73de0b244786c73787ab36c28df75b0b522b
liquidz <liquidz.uo@gmail.com>
2018-08-03 18:11:12 +0900

    add lein

---
+++ cookbooks/clojure/leiningen.rb
@@ -0,0 +1,21 @@
+include_recipe '../def'
+
+bin_dir = "#{node[:home]}/bin"
+target = "#{bin_dir}/lein"
+
+directory bin_dir do
+  owner node[:user]
+  group node[:group]
+end
+
+curl 'leiningen' do
+  path target
+  url  'https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein'
+end
+
+file target do
+  mode '0755'
+  owner node[:user]
+  group node[:group]
+end
+
