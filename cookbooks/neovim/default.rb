6c170eb847f1235461b5a6063c57805811f8a707
liquidz <liquidz.uo@gmail.com>
2018-10-03 18:14:05 +0900

    update neovim recipe

--- cookbooks/neovim/default.rb
+++ cookbooks/neovim/default.rb
@@ -1,13 +1,20 @@
 include_recipe '../python/pip3.rb'
+include_recipe '../def'
 
+version = '0.3.1'
 package 'software-properties-common'
 
-execute 'add repository' do
-  command <<-EOC
-    add-apt-repository -y -u ppa:neovim-ppa/stable
-    apt-get install neovim
-  EOC
+curl 'nvim.appimage' do
+  path '/usr/local/bin/nvim'
+  url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim.appimage"
 end
+
+# execute 'add repository' do
+#   command <<-EOC
+#     add-apt-repository -y -u ppa:neovim-ppa/stable
+#     apt-get install neovim
+#   EOC
+# end
 
 execute 'install neovim' do
   command 'pip3 install neovim'
