8ecefd167d31b275c3e52399a13d0809b6108449
liquidz <liquidz.uo@gmail.com>
2018-11-08 15:37:42 +0900

    Update zsh recipe to set dircolors

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -5,3 +5,11 @@ define :zsh_users do
 end
 
 package 'zsh'
+
+git "#{node[:home]}/src/github.com/seebi/dircolors-solarized" do
+  repository 'https://github.com/seebi/dircolors-solarized'
+end
+
+link "#{node[:home]}/.dircolors" do
+  to "#{node[:home]}/src/github.com/seebi/dircolors-solarized/dircolors.256dark"
+end
