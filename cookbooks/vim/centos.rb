3fc6c6b4380b1c6e2617c5866e4bfbaf61290b77
liquidz <liquidz.uo@gmail.com>
2019-12-12 13:44:42 +0900

    Replace vim's recipe by dad

--- cookbooks/vim/centos.rb
+++
@@ -1,5 +0,0 @@
-include_recipe 'default'
-
-vim_from_source 'latest' do
-    packages %W( make gcc git ncurses ncurses-devel python-devel ctags )
-end
--- cookbooks/vim/ubuntu.rb
+++
@@ -1,3 +0,0 @@
-include_recipe 'default'
-
-vim_from_source 'latest'
